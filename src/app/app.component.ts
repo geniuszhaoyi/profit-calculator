09603646f48d138a2434de1892c9a5f8e90c2b5a
geniuszhaoyi <genius.zhaoyi@gmail.com>
2020-05-18 23:40:41 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = "CodeSandbox";
+  stockPrice: number;
+  strikePrice: number;
+  option100Price: number;
 }
