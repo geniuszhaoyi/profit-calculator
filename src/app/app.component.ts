6021e435817e6d5782ed24a40df681f5e09bee39
Yi Zhao <genius.zhaoyi@gmail.com>
2020-05-19 01:00:05 -0400

    remaster

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,74 +1,10 @@
-import { Component } from "@angular/core";
-
-import * as c3 from "c3";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  stock: number = NaN;
-  strike: number = NaN;
-  option: number = NaN;
-
-  chart: any;
-
-  ngAfterViewInit() {
-    this.chart = c3.generate({
-      bindto: "#chart",
-      data: {
-        columns: [
-          ["data1", 30, 200, 100, 400, 150, 250],
-          ["data2", 50, 20, 10, 40, 15, 25]
-        ]
-      }
-    });
-  }
-
-  update() {}
-
-  get option100(): number {
-    return this.option * 100;
-  }
-  set option100(num: number) {
-    this.option = num / 100.0;
-  }
-
-  get stockPrice(): string {
-    return this.getString(this.stock);
-  }
-  set stockPrice(str: string) {
-    this.stock = this.getNumber(str);
-    this.update();
-  }
-  get strikePrice(): string {
-    return this.getString(this.strike);
-  }
-  set strikePrice(str: string) {
-    this.strike = this.getNumber(str);
-    this.update();
-  }
-  get option100Price(): string {
-    return this.getString(this.option100);
-  }
-  set option100Price(str: string) {
-    this.option100 = this.getNumber(str);
-    this.update();
-  }
-
-  getString(num: number): string {
-    if (num !== undefined && !isNaN(num)) {
-      return "" + num;
-    } else {
-      return "";
-    }
-  }
-  getNumber(str: string): number {
-    if (str === "") {
-      return NaN;
-    } else {
-      return +str;
-    }
-  }
+  title = 'pc';
 }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+
+import * as c3 from "c3";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  stock: number = NaN;
+  strike: number = NaN;
+  option: number = NaN;
+
+  chart: any;
+
+  ngAfterViewInit() {
+    this.chart = c3.generate({
+      bindto: "#chart",
+
+      data: {
+        xs: {
+          "stock price": 'x1',
+        },
+        columns: [
+          ["x1", 30, 200, 100, 400, 150, 250],
+          ["stock price", 30, 200, 100, 400, 150, 250]
+        ]
+      }
+    });
+  }
+
+  update() {
+    return;
+    this.chart.unload({
+      ids: ["stock price"]
+    });
+    this.chart.load({
+      columns: [["stock price", 30, 200, 100, 400, 150, 250]]
+    });
+  }
+
+  get option100(): number {
+    return this.option * 100;
+  }
+  set option100(num: number) {
+    this.option = num / 100.0;
+  }
+
+  get stockPrice(): string {
+    return this.getString(this.stock);
+  }
+  set stockPrice(str: string) {
+    this.stock = this.getNumber(str);
+    this.update();
+  }
+  get strikePrice(): string {
+    return this.getString(this.strike);
+  }
+  set strikePrice(str: string) {
+    this.strike = this.getNumber(str);
+    this.update();
+  }
+  get option100Price(): string {
+    return this.getString(this.option100);
+  }
+  set option100Price(str: string) {
+    this.option100 = this.getNumber(str);
+    this.update();
+  }
+
+  getString(num: number): string {
+    if (num !== undefined && !isNaN(num)) {
+      return "" + num;
+    } else {
+      return "";
+    }
+  }
+  getNumber(str: string): number {
+    if (str === "") {
+      return NaN;
+    } else {
+      return +str;
+    }
+  }
+}
